9e9777b2b6f832b6b7f797c7b4d9b48d22a33959
unknown <aditya.91.singh@gmail.com>
2019-07-26 09:06:06 +0100

    label geenrator is required

--- cifarGenerator.py
+++ cifarGenerator.py
@@ -3,10 +3,12 @@ __version__ = "0.1"
 
 import keras
 import numpy as np
-np.random.seed(0)
 from keras.datasets import cifar100
 from keras.layers import Input
 from keras.models import Model
+from keras import backend as K
+from utils.utils import bb_intersection_over_union
+np.random.seed(0)
 
 class CifarGenerator(keras.utils.Sequence):
 
@@ -66,10 +68,31 @@ class CifarGenerator(keras.utils.Sequence):
         return [ps, ts], ys
 
 
+def get_pretrained_model(num_channels=3):
+    mv2 = keras.applications.mobilenet.MobileNet(alpha=1.0, include_top=False, weights='imagenet', pooling=None)
+    model = Model(input=Input(shape=(None, None, num_channels)), output=mv2.output)
+    print(model.summary())
+    return model
+
+
+def getLayerIndexByName(model, layername):
+    for idx, layer in enumerate(model.layers):
+        if layer.name == layername:
+            return idx
+
+
+def model_output(model, layer_name, inp):
+    output = (model.layers[getLayerIndexByName(model, layer_name)]).output
+    functor = K.function([model.input, K.learning_phase()], output)
+    l_out = functor(inp, 0)
+    print(l_out)
+    return l_out
+
+
 class CifarFeatureGenerator(keras.utils.Sequence):
 
     def __data_generation(self, index):
-        return self.generate_labels(self.data[index*self.batch_size:(index+1)*self.batch_size,], index*self.batch_size)
+        return self.generate_labels(self.data[index])
 
     def __getitem__(self, index):
         X, y = self.__data_generation(index)
@@ -78,15 +101,10 @@ class CifarFeatureGenerator(keras.utils.Sequence):
     def __len__(self):
         return int(self.num_samples/self.batch_size)
 
-    def get_pretrained_model(self, num_channels=3):
-        mv2 = keras.applications.mobilenet.MobileNet(alpha=1.0, include_top=False, weights='imagenet', pooling=None)
-        model = Model(input=Input(shape=(None, None, num_channels)), output=mv2.output)
-        return model
-
-    def __init__(self, mode='train', batch_size=128):
-        self.model = self.get_pretrained_model(num_channels=3)
+    def __init__(self, mode='train', batch_size=1, model=None, generation_stage='output'):
+        self.model = model
+        self.generation_stage = generation_stage
         self.channels = self.model.output.shape[-1]
-        self.SIZE = 16
         self.batch_size = batch_size
 
         if mode == 'train':
@@ -99,34 +117,32 @@ class CifarFeatureGenerator(keras.utils.Sequence):
         self.num_samples = data.shape[0]
 
     def generate_labels(self, data, index):
-        sample_shape = data[0].shape
-        n_rows = int(sample_shape[1]/self.SIZE)
-        n_cols = int(sample_shape[0]/self.SIZE)
 
-        ps = np.zeros(shape=(self.batch_size, self.SIZE, self.SIZE, 3))
-        ts = np.zeros(shape=(self.batch_size, self.SIZE, self.SIZE, 3))
+        sample_shape = data.shape
+
+        row_size = np.random.choice([16, 32])
+        col_size = np.random.choice([16, 32])
+        r_row = np.random.choice(sample_shape[0]-row_size)
+        r_col = np.random.choice(sample_shape[0]-col_size)
+
+        template = data[r_row:r_row+row_size, r_col:r_col+col_size, :]
+
+        p_f_out = model_output(self.model, self.generation_stage, data)
+        p_f_shape = p_f_out.shape
+
+        t_f_out = model_output(self.model, self.generation_stage, template)
+        t_f_shape = t_f_out.shape
+
         ys = np.zeros(shape=(self.batch_size, 1))
 
-        for sample in range(self.batch_size):
-            r_id = np.random.randint(0, self.SIZE*(n_rows-1))
-            c_id = np.random.randint(0, self.SIZE*(n_cols-1))
-            t = data[sample][r_id:r_id+self.SIZE, c_id:c_id+self.SIZE,:]
+        shape_diff = [i-j for i, j in zip(p_f_shape, t_f_shape)]
 
-            if np.random.random() < 0.5:
-                r_ind = np.random.randint(0, self.batch_size)
-            else:
-                r_ind = sample
+        gt_row = np.random.choice(shape_diff[0])
+        gt_col = np.random.choice(shape_diff[1])
 
-            p = data[r_ind][r_id:r_id+self.SIZE, c_id:c_id+self.SIZE, :]
-            if r_ind == sample:
-                self.labels[sample+index] = [p, t, 1]
-                y = 1
-            else:
-                y = 0
-                self.labels[sample+index] = [p, t, 0]
+        mathcing_parent = p_f_out[gt_row:gt_row+t_f_shape[0], gt_col:gt_col+t_f_shape[1], :]
 
-            ps[sample, ] = p
-            ts[sample, ] = t
-            ys[sample, ] = y
+        approx_org_row = reduction_factor/2 +
+        iou = bb_intersection_over_union(())
 
         return [ps, ts], ys
